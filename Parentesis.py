532038af0c76a26fa1837aa96eb632dc44ca9446
David-Hernan <71787287+David-Hernan@users.noreply.github.com>
2021-08-10 17:38:22 -0500

    Actualización de la tarea

--- Parentesis.py
+++ Parentesis.py
@@ -1,3 +1,5 @@
+#David Hernán García Fernández - A01173130
+
 # Recibes una string conteniendo solo () y []
 # Crea un algoritmos para validar que la sintaxis de los corchetes es correcta:
 # Ejemplos validos:
@@ -10,6 +12,7 @@
 # '(([))]'
 # '(((((((((((((((((((((((((((((((((('
 
+"""Casos de prueba"""
 #Correctos
 #arreglo = ['(', ')']
 #arreglo = ['(', ')','(', ')','(', ')','(', ')','(', ')','(', ')','(', ')','(', ')','(', ')']
@@ -20,29 +23,31 @@ arreglo = ['(', '[', ']', ')', '[', ']', '(', ')']
 #arreglo = ['(', '(', '[', ')', ')', ']']
 #arreglo = ['(','(','(','(','(','(','(','(','(','(','(','(','(','(','(','(','(','(','(','(','(','(','(','(','(','(','(','(','(','(','(','(','(','(']
 
+def parentesis (arreglo):
+    """Función que recibe un arreglo de caracteres y revisa si los corchetes que se abrieron se cerraron.
+    Regresa True cuando encuentra errores de sintaxis en los corchetes, es decir, si alguno se abrió y
+    nunca se cerró o si se cerró sin abrirse"""
+    stack = []
+    for i in range(0,len(arreglo)): #Recorre el arreglo
+        if arreglo[i] == '(' or arreglo[i] == '[':
+            stack.append(arreglo[i]) #Guarda los corchetes en el Stack
+        elif arreglo[i] == ']':
+            if len(stack) == 0: #Si el stack está vacío y encontró un corchete cerrado es porque falta uno de apertura
+                return True #True implica que la sintaxis es incorrecta
+            elif(stack.pop() != '['): #Si no extrae el de apertura esperado
+                return True
+        elif arreglo[i] == ')':
+            if len(stack) == 0:
+                return True
+            elif(stack.pop() != '('):
+                return True
+    if len(stack) == 0: #Verifica que el Stack esté vacío al terminar las iteraciones. No estar vacío implica que abró un corchete que nunca cerró
+        return False
+    else: 
+        return True
 
-stack = []
-error = False
 
-for i in range(0,len(arreglo)):
-    if arreglo[i] == '(' or arreglo[i] == '[':
-        stack.append(arreglo[i])
-    elif arreglo[i] == ']':
-        if len(stack) == 0:
-            error = True
-            break
-        elif(stack.pop() != '['):
-            error = True
-            break
-    elif arreglo[i] == ')':
-        if len(stack) == 0:
-            error = True
-            break
-        elif(stack.pop() != '('):
-            error = True
-            break
-
-if error == False and len(stack) == 0:
-    print("Sintaxis de los corchetes CORRECTA")
+if parentesis(arreglo) == False:
+    print("Sintaxis CORRECTA en los corchetes")
 else:
-    print("Sintaxis de los corchetes INCORRECTA")
+    print("Sintaxis INCORRECTA en los corchetes")
